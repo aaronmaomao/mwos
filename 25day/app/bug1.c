9154194715273dc32cb015079db565f329efe904
Mao Zhengjun(毛正军) <mao-zhengjun@rdb.adwin.renesas.com>
2018-01-09 17:25:19 +0800

    add beep function

---
+++ 25day/app/bug1.c
@@ -0,0 +1,14 @@
+void api_putchar(int c);
+void api_end(void);
+
+void HariMain(void)
+{
+	char a[100];
+	//a[10] = 'A';
+	api_putchar(a[10]);
+	//a[100] = 'B';
+	api_putchar(a[100]);
+	//a[100] = 'C';
+	api_putchar(a[123]);	//栈异常
+	api_end();
+}
