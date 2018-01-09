55eeba93cbd385cc6f4d079e93e4808efdd5bc7a
aaronmaomao <1076249537@qq.com>
2018-01-09 21:33:30 +0800

    update

--- 25day/app/noodle.c
+++ 25day/app/noodle.c
@@ -24,7 +24,7 @@ void api_end(void);
 void HariMain(void) {
 	int i, timer;
 	timer = api_alloctimer();
-	api_initmalloc(timer, 128);
+	api_inittimer(timer, 128);
 	for (i = 20000000; i >= 20000; i -= i / 100) {
 		api_beep(i);
 		api_settimer(timer, 1);
